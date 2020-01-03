63fbdf5d484286feefb1c03229c573289da17cc3
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 14:06:34 +0600

    Updates

---
+++ precache-manifest.db02ba430d211b2be857d597b1621e6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d084581ced9aee83b50b145c4569d6",
+    "url": "/sm-pokemon/index.html"
+  },
+  {
+    "revision": "f14ea741474c2d97f45e",
+    "url": "/sm-pokemon/static/css/main.1e304baf.chunk.css"
+  },
+  {
+    "revision": "99532aa78cbe71039d89",
+    "url": "/sm-pokemon/static/js/2.9f17cd95.chunk.js"
+  },
+  {
+    "revision": "f14ea741474c2d97f45e",
+    "url": "/sm-pokemon/static/js/main.bac849e7.chunk.js"
+  },
+  {
+    "revision": "ce14cc97043ee27f285d",
+    "url": "/sm-pokemon/static/js/runtime-main.f3bb7b0d.js"
+  }
+]);
